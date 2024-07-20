e49486a6cbee85efe66d7470c1496046c00eed75
Benjamin Gudehus <hastebrot@gmail.com>
2024-07-20 05:08:42 +0200

    feat: Search suggestions component added.

--- domains/fashion/applications/fashion-ui/api/site.tsx
+++ domains/fashion/applications/fashion-ui/api/site.tsx
@@ -145,10 +145,10 @@ const ShoppingInteractions = () => {
 
 const ShoppingSearch = () => {
   return (
-    <div class="w-[440px] relative">
+    <div class="w-[440px] relative group">
       <input
         class={classNames(
-          "rounded-[2px] p-[10px] h-[48px] w-full [outline:0]",
+          "peer rounded-[2px] p-[10px] h-[48px] w-full [outline:0]",
           "[box-shadow:0_0_0_1px_#676767] text-[#1c1c1c] placeholder:text-[#858585]",
           "hover:[box-shadow:0_0_0_2px_#1c1c1c] hover:bg-[#f1f1f1]",
           "focus:[box-shadow:0_0_0_2px_#1c1c1c] focus:bg-[#fff]"
@@ -156,12 +156,103 @@ const ShoppingSearch = () => {
         placeholder="Suche"
       />
       <button
-        class="absolute right-0 inset-y-0 flex items-center justify-center text-[#1c1c1c] w-[48px] h-[48px] pointer-events-none"
+        class={classNames(
+          "absolute right-0 inset-y-0 flex items-center justify-center",
+          "w-[48px] h-[48px] rounded-r-[2px]",
+          "peer-placeholder-shown:bg-transparent peer-placeholder-shown:text-[#1c1c1c]",
+          "peer-placeholder-shown:pointer-events-none",
+          "bg-[#cc071e] hover:bg-[#9e0012] text-[#fff]"
+        )}
         type="submit"
       >
-        <icons.Search class="w-[24px] h-[24px]" />
+        <icons.Search class="w-[24px] h-[24px] shrink-0" />
       </button>
+      <div
+        class={classNames(
+          "hidden group-focus-within:block peer-placeholder-shown:hidden",
+          "absolute top-[calc(48px+1px)] left-0 w-[40rem] z-50"
+        )}
+        tabIndex={-1}
+      >
+        <ShoppingSearchSuggestions />
+      </div>
     </div>
+  );
+};
+
+const ShoppingSearchSuggestions = () => {
+  return (
+    <section
+      class={classNames(
+        "shopping-search-suggestions bg-[#fff] border border-[#ccc]",
+        "flex flex-col",
+        "[box-shadow:0_4px_8px_0_rgba(0,0,0,0.2),0_6px_20px_0_rgba(0,0,0,0.19)]"
+      )}
+    >
+      <ul>
+        <ShoppingSearchProduct
+          productTitle="Pfanner Multivitamin ACE 1,5l (EINWEG)"
+          productGrammage="1,50l (1 l = 1,26 €) zzgl. 0,25 € Pfand"
+          amount={0}
+        />
+        <ShoppingSearchProduct
+          productTitle="Beckers Bester Frühstücksvitamine ACE 1l (MEHRWEG)"
+          productGrammage="1l zzgl. 0,15 € Pfand"
+          amount={1}
+        />
+        <ShoppingSearchProduct
+          productTitle="Valensina Frühstücks-ACE 1l (EINWEG)"
+          productGrammage="1l zzgl. 0,25 € Pfand"
+          amount={2}
+        />
+      </ul>
+
+      <div class="w-full bg-[#f1f1f1] p-[16px]">
+        <a
+          class={classNames(
+            "font-[400] text-[16px] leading-[1.5] flex items-center justify-center",
+            "h-[48px] w-full max-w-[288px] rounded-[2px] bg-[#cc071e] text-[#fff] m-auto",
+            "hover:bg-[#9e0012] cursor-pointer"
+          )}
+        >
+          Alle Suchergebnisse anzeigen
+        </a>
+      </div>
+    </section>
+  );
+};
+
+type ShoppingSearchSuggestionProps = {
+  productTitle: string;
+  productGrammage: string;
+  amount: number;
+};
+
+const ShoppingSearchProduct = (props: ShoppingSearchSuggestionProps) => {
+  return (
+    <li class="flex border-b border-[#ccc]">
+      <div class="w-[120px] h-[120px]">
+        <a class="my-[16px]">
+          <span class="flex items-center justify-center w-full h-full">
+            <icons.Image class="w-[72px] h-[72px] text-[#949494] [stroke-width:calc(2px*24/72)]" />
+          </span>
+        </a>
+      </div>
+      <div class="w-[calc(100%-120px-200px)] flex flex-col pr-[16px]">
+        <a class="font-[500] text-[#1c1c1c] text-[18px] leading-[1.3] mt-[16px] mb-[8px]">
+          {props.productTitle}
+        </a>
+        <div class="font-[400] text-[#1c1c1c] text-[14px]">{props.productGrammage}</div>
+      </div>
+      <div class="w-[200px] m-[16px] ml-0 flex flex-col justify-between">
+        <div class="font-[500] text-[18px] text-[#1c1c1c] whitespace-nowrap text-right mb-[8px]">
+          0,00 €
+        </div>
+        <div class="w-full">
+          <ProductActions amount={props.amount} />
+        </div>
+      </div>
+    </li>
   );
 };
 
@@ -709,7 +800,9 @@ const Product = (props: ProductProps) => {
           productOfferPrice={props.productOfferPrice}
         />
       </a>
-      <ProductActions amount={props.amount} />
+      <div class="m-[8px_16px_16px]">
+        <ProductActions amount={props.amount} />
+      </div>
     </div>
   );
 };
@@ -807,14 +900,12 @@ type ProductActionsProps = {
 
 const ProductActions = (props: ProductActionsProps) => {
   return (
-    <div class="product-actions h-[40px] m-[8px_16px_16px]">
-      <div class="flex flex-nowrap items-start content-stretch justify-between">
-        <div>
-          <ProductFavoriteButton />
-        </div>
-        <div>
-          <ProductBasketButton amount={props.amount} />
-        </div>
+    <div class="product-actions h-[40px] flex flex-nowrap items-start content-stretch justify-between">
+      <div>
+        <ProductFavoriteButton />
+      </div>
+      <div>
+        <ProductBasketButton amount={props.amount} />
       </div>
     </div>
   );
