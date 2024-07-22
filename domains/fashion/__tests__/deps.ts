7ac4f34500c1e96229fdff93613d6a6d575046f6
Benjamin Gudehus <hastebrot@gmail.com>
2024-07-22 06:42:14 +0200

    refactor: Shop domain renamed. (#10)

--- domains/fashion/__tests__/deps.ts
+++
@@ -1,2 +0,0 @@
-export * as fake from "@ngneat/falso";
-export * as assert from "https://deno.land/std@0.224.0/assert/mod.ts";
---
+++ domains/shop/__tests__/deps.ts
@@ -0,0 +1,2 @@
+export * as fake from "@ngneat/falso";
+export * as assert from "@std/assert/mod.ts";
