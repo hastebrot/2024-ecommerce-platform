22f77ada25c7572563e8b5eb63bb5c0270cbb238
Benjamin Gudehus <hastebrot@gmail.com>
2024-07-23 22:14:40 +0200

    feat: Basket overview added.

--- domains/shop/services/shop-ui/api/components/ShoppingHeader.tsx
+++ domains/shop/services/shop-ui/api/components/ShoppingHeader.tsx
@@ -39,7 +39,7 @@ export const ShoppingSearch = () => {
       <div
         class={classNames(
           "hidden group-focus-within:block peer-placeholder-shown:hidden",
-          "absolute top-[calc(48px+1px)] left-0 w-[40rem] z-50"
+          "absolute top-[calc(48px+1px)] left-[-2px] w-[40rem] z-50"
         )}
         tabIndex={-1}
       >
@@ -164,8 +164,10 @@ export const ShoppingNavigationItemBasket = (props: ShoppingNavigationItemBasket
         "group flex h-[3rem] min-w-[8.5625rem] rounded-[0.125rem] text-[#1c1c1c] bg-[#fff] px-[0.5rem] border border-[#ccc] items-center justify-start",
         !props.isFilled &&
           "hover:bg-[#f1f1f1] hover:border-[#1c1c1c] hover:[box-shadow:inset_0_0_0_1px_#1c1c1c]",
-        props.isFilled && "!bg-[#007d3e] !border-[#007d3e] !text-[#fff]",
-        props.isFilled && "hover:!bg-[#005c2e] hover:!border-[#005c2e]"
+        props.isFilled && [
+          "!bg-[#007d3e] !border-[#007d3e] !text-[#fff]",
+          "hover:!bg-[#005c2e] hover:!border-[#005c2e]",
+        ]
       )}
     >
       <span class="relative">
